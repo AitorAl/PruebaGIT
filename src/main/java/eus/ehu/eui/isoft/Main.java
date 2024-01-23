38c1567a9365d40377980cd6010e11b7eb14f338
Aitor Alvarez <aalvarez175@ikasle.ehu.eus>
2024-01-23 17:23:42 +0100

    Creando metodo 2 en la rama de pruebas

--- src/main/java/eus/ehu/eui/isoft/Main.java
+++ src/main/java/eus/ehu/eui/isoft/Main.java
@@ -3,11 +3,18 @@ package eus.ehu.eui.isoft;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //llamada al metodo 2
+        method2();
     }
-
 
     public static void method1(){
         System.out.println("Esto es un método");
+
+    }
+
+    public static void method2()
+    {
+            System.out.println("Otro método");
 
     }
 }
