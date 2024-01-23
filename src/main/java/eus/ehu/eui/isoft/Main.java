8aa801012a8805605b09f549f5651edaa545637e
Aitor Alvarez <aalvarez175@ikasle.ehu.eus>
2024-01-23 17:21:24 +0100

    Añadido el metodo

--- src/main/java/eus/ehu/eui/isoft/Main.java
+++ src/main/java/eus/ehu/eui/isoft/Main.java
@@ -4,4 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    public static void method1(){
+        System.out.println("Esto es un método");
+
+    }
 }
